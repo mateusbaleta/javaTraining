1d1e827764245a7d8024dfcef3fe345f887cca19
NukeMachine <nukemachine@localhost.localdomain>
2022-12-08 01:23:35 -0300

    basics

---
+++ basics/HelloWorld.java
@@ -0,0 +1,11 @@
+package basics;
+
+/**
+ * No hello world, no programmer luck
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
